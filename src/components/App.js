647e4cf7da180271976ca244978956b9a9e5116e
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-05-17 10:45:41 -0300

    Search an Artists Top Tracks

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com'
+
 class App extends Component {
 
   state = {
-    artistQuery: ''
+    artistQuery: '',
+    artist: null,
+    tracks: []
   }
 
   updateArtistQuery = (e) => {
@@ -11,7 +15,20 @@ class App extends Component {
   }
 
   searchArtist = () => {
-    console.log(this.state)
+    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+      .then(response => response.json())
+      .then(json => {
+        if(json.artists.total > 0){
+          const artist = json.artists.items[0]
+          this.setState({ artist })
+
+          fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+            .then(response => response.json())
+            .then(json => this.setState({ tracks: json.tracks }))
+            .catch(error => alert(error.message))
+        }
+      })
+      .catch(error => alert(error.message))
   }
 
   handleKeyPress = (e) => {
@@ -21,6 +38,7 @@ class App extends Component {
   }
 
   render() {
+    console.log(this.state)
     return(
       <div>
         <h2>Music Master</h2>
