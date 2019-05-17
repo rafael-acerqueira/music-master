3852799b813cf2c1c0926c92b562906c89437a0b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-05-17 10:08:42 -0300

    Setup application

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+
+  render() {
+    return(
+      <div>
+        React
+      </div>
+    )
+  }
+}
+
+export default App
